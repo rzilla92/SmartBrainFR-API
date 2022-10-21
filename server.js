5fc3fd8932ff12fed722c6f7d77bbf35d6413164
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-10-21 10:08:24 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '3119107',
+      database : 'smart-brain'
+    }
+})
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen (port, ()=> {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
+
+// / --> res = this is working
+// /register --> POST = user
+// /profile/:userId --> GET = user
+// /image --> PUT --> user
