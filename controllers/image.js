147f985edd3011109f2621f3417dca95834d7376
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2023-01-25 11:01:22 +0700

    update API call

--- controllers/image.js
+++ controllers/image.js
@@ -5,12 +5,20 @@ const app = new Clarifai.App({
    })
 
 const handleApiCall = (req, res) => {
+
     app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict(
+      {
+        id: 'face-detection',
+        name: 'face-detection',
+        version: '6dc7e46bc9124c5c8824be4822abe105',
+        type: 'visual-detector',
+      }, req.body.input)
     .then(data => {
       res.json(data);
     })
     .catch(err => res.status(400).json('unable to work with API'))
+
 }
 
 const handleImage = (req,res,db) => {
